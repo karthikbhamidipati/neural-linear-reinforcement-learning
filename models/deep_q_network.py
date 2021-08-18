e47a4c6db45b490e3da02ccccb6a3503dbc958af
Karthik Bhamidipati <bssvkarthik@gmail.com>
2021-08-18 17:29:08 +0100

    [ch93] - Final changes to add evaluation (#7)
    * [ch93] - Reusable and Rerunnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code
    
    * [ch93] - Reusable and Re-runnable code

--- models/deep_q_network.py
+++ models/deep_q_network.py
@@ -2,7 +2,7 @@ from os.path import exists
 
 from keras import Sequential
 from keras.initializers import glorot_uniform, glorot_normal, he_uniform, he_normal
-from keras.layers import Conv2D, Input, Flatten
+from keras.layers import Conv2D, Input, Flatten, MaxPooling2D
 from keras.models import Model, load_model
 
 _init_strategy_dict = {
@@ -36,9 +36,28 @@ def _pretrained_dqn(weights_dir):
     return Model(inputs=model.input, outputs=model.layers[-3].output)
 
 
+def _dqn_pooling():
+    dqn_pooling_model = Sequential(
+        [
+            Input(shape=(84, 84, 4), name="input"),
+            Conv2D(32, 8, activation="relu", name="conv_1", kernel_initializer=glorot_normal()),
+            MaxPooling2D(8, 4),
+            Conv2D(64, 4, activation="relu", name="conv_2", kernel_initializer=glorot_normal()),
+            MaxPooling2D(4, 2),
+            Conv2D(64, 3, activation="relu", name="conv_3", kernel_initializer=glorot_normal()),
+            MaxPooling2D(3, 1),
+            Flatten()
+        ]
+    )
+
+    return dqn_pooling_model
+
+
 def get_dqn_extractor(init_strategy: str = 'glorot_uniform', weights_dir: str = None) -> Model:
     if init_strategy == 'pretrained':
         return _pretrained_dqn(weights_dir)
+    elif init_strategy == 'pooling':
+        return _dqn_pooling()
     elif init_strategy in _init_strategy_dict:
         return _random_init_dqn(init_strategy)
     else:
